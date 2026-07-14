3cd9caccd1e52e204135f695a5adb3932dbdcc3e
mcenahle <hi@mcenahle.page>
2026-07-14 21:44:33 +0800

    feat: 实装复制种子功能，引入 iconify 图标

--- assets/js/components.js
+++ assets/js/components.js
@@ -17,8 +17,26 @@ function loadScript(src) {
   document.body.appendChild(script);
 }
 
+// 复制存档种子
+
+function copySaveSeed() {
+  navigator.clipboard.writeText(document.getElementById("seed").innerText);
+
+  document.getElementById("copy-btn").innerHTML =
+    "<iconify-icon icon='material-symbols:check-rounded'></iconify-icon> 已复制";
+
+  setTimeout(function () {
+    document.getElementById("copy-btn").innerHTML =
+      "<iconify-icon icon='si:copy-line'></iconify-icon> 复制";
+  }, 2000);
+}
+
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js");
 
 loadScript(
   "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js",
+);
+
+loadScript(
+  "https://code.iconify.design/iconify-icon/3.0.0/iconify-icon.min.js",
 );
